ed6419fc134c799f80752611d32a922c40caa67f
nzxi <fnsvc.0@gmail.com>
2025-11-08 21:45:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,32 @@ TITLE = r"""
 """
 
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
 def convert_thatshit():
     input_image = input("Which image do you wanna convert?: ")
     output_image = input("Save image as: ")
     convert_image(input_image, output_image)
+    global message
     message = f"Saved image as: {output_image}"
 
 
 def main():
-    print(TITLE)
-    print("Available Options:")
-    print(" 1. Image conversion")
-    choice = input("Choose an option: ")
+    while True:
+        clear_screen()
+        print(TITLE)
+        print("Available Options:")
+        print(" 1. Image conversion")
+        choice = input("Choose an option: ")
 
-    if choice == '1':
-        convert_thatshit()
+        if choice == '1':
+            convert_thatshit()
+            clear_screen()
+            print(message)
+            input("Press any key to continue...")
 
 if __name__ == '__main__':
-    while True:
         main()
